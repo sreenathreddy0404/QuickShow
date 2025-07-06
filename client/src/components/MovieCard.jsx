13c2e8262c508460cffcada3c25e6e2bd6d49c1a
sreenathreddy0404 <sreenathreddyannareddy@gmail.com>
2025-07-06 23:21:10 +0530

    added trailers section

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 import { StarIcon } from 'lucide-react';
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import timeFormat from '../lib/timeFormat';
 
 const MovieCard = ({movie}) => {
     const navigate = useNavigate();
@@ -17,20 +18,20 @@ const MovieCard = ({movie}) => {
 			/>
 			<p className="font-semibold mt-2 truncate">{movie.title}</p>
 			<p className="text-sm text-gray-400 mt-2">
-				{new Date(movie.release_data).getFullYear()} .{" "}
+				{new Date(movie.release_date).getFullYear()} • {" "}
 				{movie.genres
 					.slice(0, 2)
 					.map((genre) => genre.name)
 					.join(" | ")}{" "}
-				. {movie.runtime}
+				• {timeFormat(movie.runtime)}
 			</p>
-			<div className='flex justify-between items-center mt-2'>
+			<div className="flex justify-between items-center mt-2">
 				<button
 					onClick={() => {
 						navigate(`/movies/${movie._id}`);
 						scrollTo(0, 0);
 					}}
-					 className="px-4 py-2 text-xs bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+					className="px-4 py-2 text-xs bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
 				>
 					Buy Tickets
 				</button>
