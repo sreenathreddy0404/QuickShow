d73f2caafe304e576fd1ae167addac9c41268a36
sreenathreddy0404 <sreenathreddyannareddy@gmail.com>
2025-07-11 22:37:55 +0530

    dummy commit

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -76,7 +76,7 @@ const MovieDetails = () => {
 
       <DateSelect dateTime={show.dateTime} id={id}/>
 
-      <p className="text-lg font-medium mt-20 mb-8">You May Also Like</p>
+      <p className="text-lg font-medium mt-20 mb-8">You May Also Like </p>
       <div className="flex flex-wrap max-sm:justify-center gap-8">
         {dummyShowsData.slice(0,4).map((movie,index)=>{
           return <MovieCard key={index} movie={movie} />
