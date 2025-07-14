fab3de43070aeb2ea43589f946fcc22f4ab7134a
sreenathreddy0404 <sreenathreddyannareddy@gmail.com>
2025-07-14 23:14:50 +0530

    dummy commit

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { assets, dummyDateTimeData, dummyShowsData } from '../assets/assets';
 import Loading from '../components/Loading';
-import { ArrowRightIcon, ClockIcon } from 'lucide-react';
-import timeFormat from '../lib/timeFormat';
+import { ArrowRightIcon, ClockIcon } from 'lucide-react'
 import isoTimeFormat from '../lib/isoTimeFormat'
 import BlurCircle from '../components/BlurCircle'
 import toast from 'react-hot-toast'
