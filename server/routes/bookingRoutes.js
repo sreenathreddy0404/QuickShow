caff9fd637d64d0badb252a0272d06a21c0918b4
sreenathreddy0404 <sreenathreddyannareddy@gmail.com>
2025-07-28 18:23:12 +0530

    corrected the spelling in booking routes

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createBooking, getOccupiedSeats } from '../controllers/BookingController.js';
+import { createBooking, getOccupiedSeats } from '../controllers/bookingController.js';
 
 const bookingRouter = express.Router();
 
